a1de97846d695cd257ce850dd53b64759b29f6a2
Talal Talhouk <talal@sbcglobal.net>
2017-08-07 12:03:18 -0700

    add logging

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,12 @@ class User < ApplicationRecord
   devise :omniauthable, :omniauth_providers => [:facebook]
 
   def self.from_omniauth(auth)
-   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-     user.email = auth.info.email
-     user.password = Devise.friendly_token[0,20]
-   end
+    p 'auth args'
+    p auth
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
   end
   def apply_omniauth(auth)
   	update_attributes(
